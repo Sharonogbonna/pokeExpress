4415a480218c69ad4b5ab1af0bba7b8d96b2ad30
Sharon Ogbonna <114447309+Sharonogbonna@users.noreply.github.com>
2022-12-02 14:05:03 -0600

    some backfround for the show

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,10 +1,14 @@
 const React = require('react')
+const myStyle = {
+   color: '#ffffff',
+   backgroundColor: '#000000',
+ };
 class Show extends React.Component {
    render () {
       const clickedPokemon = this.props.chosenPokemon
       return (// you can only return one element but one element can have baby elements so a div etc
         <div>
-         <link rel="stylesheet" type="text/css" href="css/style.css"/>
+         <body style={myStyle}></body>
          <h1>Gotta Catch 'Em All</h1>
          <h2>{clickedPokemon.name.toUpperCase()}</h2>
          <img src={clickedPokemon.img + '.jpg'} alt="picture of pokemon" />
